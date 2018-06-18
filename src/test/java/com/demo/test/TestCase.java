07021cb98c48b88031761a20638fdc0186be95e8
Deepak Kumar <ms.deepak456@gmail.com>
2018-06-18 21:52:41 +0530

    Update TestCase.java

--- src/test/java/com/demo/test/TestCase.java
+++ src/test/java/com/demo/test/TestCase.java
@@ -8,7 +8,54 @@ public  class TestCase {
     @Test(priority = 10, alwaysRun = true, enabled = true)
   public void testMethod()
     {
-        System.out.println("success");
+        System.out.println("success 1");
     }
+
+    @Test(priority = 10, alwaysRun = true, enabled = true)
+    public void testMethod1()
+    {
+        System.out.println("success 2");
+    }
+
+    @Test(priority = 10, alwaysRun = true, enabled = true)
+    public void testMethod2()
+    {
+        System.out.println("success 3");
+    }
+    @Test(priority = 10, alwaysRun = true, enabled = true)
+    public void testMethod3()
+    {
+        System.out.println("success 4");
+    }
+    @Test(priority = 10, alwaysRun = true, enabled = true)
+    public void testMethod4()
+    {
+        System.out.println("success 5");
+    }
+    @Test(priority = 10, alwaysRun = true, enabled = true)
+    public void testMethod5()
+    {
+        System.out.println("success 5");
+    }
+    @Test(priority = 10, alwaysRun = true, enabled = true)
+    public void testMethod6()
+    {
+        if(5<8){
+            System.out.println ("Test case Fail 6");
+        }else {
+            System.out.println ("success 6");
+        }
+    }
+    @Test(priority = 10, alwaysRun = true, enabled = true)
+    public void testMethod7()
+    {
+        System.out.println("success  7");
+    }
+    @Test(priority = 10, alwaysRun = true, enabled = true)
+    public void testMethod8()
+    {
+        System.out.println("success 8");
+    }
+
 
 }
